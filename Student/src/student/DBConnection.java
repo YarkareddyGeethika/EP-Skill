1677d0663a083c0e4340c6c9119d83eef6dbed28
YarkareddyGeethika <74001247+YarkareddyGeethika@users.noreply.github.com>
2021-01-24 12:00:44 +0530

    Delete DBConnection.java

--- Student/src/student/DBConnection.java
+++
@@ -1,7 +0,0 @@
-package student;
-
-public class DBConnection {
-        public static connection DBConnection()
-        Class.forName("oracle.jdbc.driver.OracleDriver")
-        connection con.DriverManager.getConnection()
-}
